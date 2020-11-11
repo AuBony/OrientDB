f881367600409968cf700f63af8d0f51523194fd
Léa Pautrel <pautrel.lea@gmail.com>
2020-11-11 16:48:28 +0100

    Update création BDD : Importation de tous les records mais pas de liens

--- orientDB.py
+++ orientDB.py
@@ -37,7 +37,7 @@ if client.db_exists("tolkien"):
 
 # On créé une BDD tolkien
 client.db_create("tolkien")
-print("Base de données 'tolkien' crée.")
+print("Base de données 'tolkien' créée.")
 print("\n----- Bases de données actuelles de l'utilisateur -----")
 print(client.db_list())
 
@@ -57,36 +57,102 @@ print("\n----- Connexion à la base de données 'tolkien' -----")
 #                     "loves","hassibling", "_studio", # 15 à 17
 #                     "osecuritypolicy"]# 18
 
-list_clusters = ["CREATURE", "LOCATION","EVENT","BEGETS", "LOVES","HASSIBLING"]
+list_clusters = ["Creature", "Location","Event","BEGETS", "LOVES","HASSIBLING"]
 
+print("\n----- Création des cluster -----")
 for clust in list_clusters:
     client.data_cluster_add(clust, pyorient.CLUSTER_TYPE_PHYSICAL)
     print('Cluster physique créé : %s' % clust)
+
+
+######################
+### Création des classes
+######################
+    
+print("\n----- Création des classes -----")
+# Vertex Classes
+client.command("CREATE CLASS AbstractName EXTENDS V")
+client.command("CREATE CLASS Event EXTENDS V")
+client.command("CREATE CLASS Creature EXTENDS AbstractName")
+client.command("CREATE CLASS Location EXTENDS AbstractName")
+
+# Edge Classes
+client.command("CREATE CLASS BEGETS EXTENDS E")
+client.command("CREATE CLASS HASSIBLING EXTENDS E")
+client.command("CREATE CLASS LOVES EXTENDS E")
+print("Classes créées")
 
 ######################
 ### Création des records dans la BDD
 ######################
     
+print("\n----- Insertion des records dans la base de données -----")
+
 for k in data_lotr.get("records"):
 #    print(k) 
+    id_cluster = 0
     
-    # Choix de l'ID en fonction du nom du cluster
-    if k.get("@class") == "CREATURE":
+    # Classe Creature
+    if k.get("@class") == "Creature":
+        record_k = {"searchname": k.get("searchname"),
+                    "uniquename": k.get("uniquename"),
+                    "gender": k.get("gender"),
+                    "race": k.get("race"),
+                    "gatewaylink": k.get("gatewaylink"),
+                    "born": k.get("born"),
+                    "altname": k.get("altname"),
+#                    "in_BEGETS": k.get("in_BEGETS"),
+            		"died": k.get("died"),
+            		"significance": k.get("significance"),
+            		"out_BEGETS": k.get("out_BEGETS"),
+            		"name": k.get("name"),
+            		"location": k.get("location"),
+#            		"in_LOVES": k.get("in_LOVES"),
+            		"illustrator": k.get("illustrator"),
+                    }
         id_cluster = 18
-    elif k.get("@class") == "LOCATION":
+    elif k.get("@class") == "Location":
+        record_k = {"significance":  k.get("significance"),
+                    "area": k.get("area"),
+                    "searchname": k.get("searchname"),
+                    "uniquename": k.get("uniquename"),
+                    "gender": k.get("gender"),
+                    "race": k.get("race"),
+                    "gatewaylink": k.get("gatewaylink"),
+            		"name": k.get("name"),
+                    "altname": k.get("altname"),
+                    "type": k.get("type"),
+            		"age": k.get("age"),
+            		"canon": k.get("canon"),
+            		"illustrator": k.get("illustrator"),
+                    }
         id_cluster = 19
-    elif k.get("@class") == "EVENT":
+    elif k.get("@class") == "Event":
+        record_k = {"uniquename": k.get("uniquename"),
+            		"name": k.get("name"),
+                    "description": k.get("description"),
+            		"illustrator": k.get("illustrator"),
+                    }
         id_cluster = 20
     elif k.get("@class") == "BEGETS":
+        record_k = {"in": k.get("in"),
+            		"out": k.get("out")
+                    }
         id_cluster = 21
     elif k.get("@class") == "LOVES":
+        record_k = {"in": k.get("in"),
+            		"out": k.get("out")
+                    }
         id_cluster = 22
     elif k.get("@class") == "HASSIBLING":
+        record_k = {"in": k.get("in"),
+            		"out": k.get("out")
+                    }
         id_cluster = 23
-        
-    # Sélection des records pour lesquels @class in list_clusters
+    
+    # Insertion dans la BDD du record
     if k.get("@class") in list_clusters:
-        print(k)
+        client.record_create(id_cluster, record_k)
 
 
 
